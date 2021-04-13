4d06fe1c65621788067cf5726285cc1977bbdfc8
Maja Kabus <mkabus@cern.ch>
2021-04-13 22:29:07 +0200

    Start of multioutput inference, not working yet

--- Analysis/Tutorials/src/pidWithONNX.cxx
+++ Analysis/Tutorials/src/pidWithONNX.cxx
@@ -16,11 +16,17 @@
 
 using namespace o2;
 using namespace o2::framework;
+using namespace o2::framework::expressions;
 
 // See https://github.com/saganatt/PID_ML_in_O2 for instructions
 
 struct ApplyOnnxModelTask {
-  Configurable<std::string> onnxFileConf{"ONNX file", "/home/maja/CERN_part/CERN/PID_ML_in_O2/models/Simple_example.onnx", "ONNX file"};
+  Filter etafilter = (aod::track::trackEtaEmcal <= 1.0f) && (aod::track::trackEtaEmcal >= 0.0f);
+  Filter phifilter = (aod::track::trackPhiEmcal <= 1.0f) && (aod::track::trackPhiEmcal >= 0.0f);
+
+  using MyTracks = soa::Filtered<aod::FullTracks>;
+
+  Configurable<std::string> onnxFileConf{"onnx-file", "/home/maja/CERN_part/CERN/PID_ML_in_O2/models/Simple_example.onnx", "ONNX file"};
   std::string onnxFile = (std::string)onnxFileConf;
 
   std::vector<std::string> input_names;
@@ -32,7 +38,7 @@ struct ApplyOnnxModelTask {
   Ort::SessionOptions session_options;
   Ort::Experimental::Session session{env, onnxFile, session_options};
 
-  HistogramRegistry onnxResults{"onnxResults", {{"results", "results", {HistType::kTH1F, {{2, 0., 1.}}}}}};
+  HistogramRegistry onnxResults{"onnxResults", {{"results", "results", {HistType::kTH1F, {{5, 0., 4.}}}}}};
 
   // pretty prints a shape dimension vector
   std::string print_shape(const std::vector<int64_t>& v)
@@ -64,25 +70,24 @@ struct ApplyOnnxModelTask {
     }
 
     // Assume model has 1 input node and 1 output node.
-    assert(input_names.size() == 1 && output_names.size() == 1);
+    //assert(input_names.size() == 1 && output_names.size() == 1);
   }
 
-  void process(aod::Tracks const& tracks)
+  void process(MyTracks const& tracks)
   {
     auto input_shape = input_shapes[0];
     for (auto& track : tracks) {
-      LOGF(INFO, "collision id: %d; eta: %.3f; phi: %.3f; x: %.3f, y: %.3f, z: %.3f",
-           track.collisionId(), track.eta(), track.phiraw(), track.x(), track.y(), track.z());
+      // Just some random values that are close to [0, 1]
+      LOGF(INFO, "collision id: %d; EMCAL eta: %.3f; EMCAL phi: %.3f; Sigma Y: %.3f, Sigma Z: %.3f, Sigma 1Pt: %.3f",
+           track.collisionId(), track.trackEtaEmcal(), track.trackPhiEmcal(), track.sigmaY(), track.sigmaZ(), track.sigma1Pt());
 
-      std::vector<float> input_tensor_values{track.eta(), track.phiraw(), track.x(), track.y(), track.z()};
+      std::vector<float> input_tensor_values{track.trackEtaEmcal(), track.trackPhiEmcal(), track.sigmaY(), track.sigmaZ(), track.sigma1Pt()};
       std::vector<Ort::Value> input_tensors;
       input_tensors.push_back(Ort::Experimental::Value::CreateTensor<float>(input_tensor_values.data(), input_tensor_values.size(), input_shape));
 
       // double-check the dimensions of the input tensor
-      assert(input_tensors[0].IsTensor()
-               input_tensors[0]
-                 .GetTensorTypeAndShapeInfo()
-                 .GetShape() == input_shape);
+      assert(input_tensors[0].IsTensor() &&
+             input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_shape);
       LOG(INFO) << "input_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape());
 
       // pass data through model
@@ -90,18 +95,22 @@ struct ApplyOnnxModelTask {
       try {
         auto output_tensors = session.Run(input_names, input_tensors, output_names);
         LOG(INFO) << "done";
-        //LOG(INFO) << "Number of output tensors: " << output_tensors.size();
+        
+        LOG(INFO) << "Number of output tensors: " << output_tensors.size();
 
         // double-check the dimensions of the output tensors
         // NOTE: the number of output tensors is equal to the number of output nodes specifed in the Run() call
+        //
+        // Check: https://github.com/microsoft/onnxruntime/issues/979
         assert(output_tensors.size() == output_names.size() &&
                output_tensors[0].IsTensor());
         LOG(INFO) << "output_tensor_shape: " << print_shape(output_tensors[0].GetTensorTypeAndShapeInfo().GetShape());
 
         for (auto& output : output_tensors) {
-          const float* output_value = output.GetTensorData<float>();
-          LOG(INFO) << "output: " << *output_value;
-          onnxResults.get<TH1>(HIST("results"))->Fill(*output_value);
+          LOG(INFO) << "NEXT OUTPUT";
+          float* output_values = output.GetTensorMutableData<float>();
+          LOG(INFO) << "output: " << *(output_values);
+          onnxResults.get<TH1>(HIST("results"))->Fill(*(output_values));
         }
 
       } catch (const Ort::Exception& exception) {
